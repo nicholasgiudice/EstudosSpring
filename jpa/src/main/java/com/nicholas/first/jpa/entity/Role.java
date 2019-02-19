28e4aaadc36ddd47c253a6f4a08a223b1b00767d
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-02-18 21:33:21 -0300

    First Project. JPA With H2

---
+++ jpa/src/main/java/com/nicholas/first/jpa/entity/Role.java
@@ -0,0 +1,31 @@
+package com.nicholas.first.jpa.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String Name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
